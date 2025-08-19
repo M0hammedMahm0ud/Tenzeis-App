6a4409a9e3dffe49a77a5c76a08b2d35140d7453
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-08-19 17:35:40 +0300

    add logic

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,3 +1,11 @@
 export default function Die(props) {
-  return <button>{props.value}</button>;
+  return (
+    <button
+      onClick={() =>
+        props.setIsClicked((prev) => [...prev, { id: props.id, clicked: true }])
+      }
+    >
+      {props.value}
+    </button>
+  );
 }
